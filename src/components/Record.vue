700237fbcb9be4d79d79a31d93abd188d9f2126d
Enrique <u20201f172@upc.edu.pe>
2023-11-20 14:14:15 -0500

    serv9ce

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -10,66 +10,43 @@
         </router-link>
       </div>
     </div>
-
-    <Pv-DataTable :value="datas" tableStyle="min-width: 50rem">
-      <Pv-Column field="id" header="Id" sortable style="width: 10%" />
-      <Pv-Column field="image_front_url" header="Imagen" style="width: 20%">
-        <template #body="slotProps">
-          <img :src="slotProps.rowData.image_front_url" alt="Imagen del producto" />
-        </template>
-      </Pv-Column>
-      <Pv-Column field="categories" header="Categoría" style="width: 20%">
-        <template #body="slotProps">
-          <p v-if="slotProps.rowData.categories">{{ slotProps.rowData.categories[0] }}</p>
-        </template>
-      </Pv-Column>
-      <Pv-Column field="stores" header="Tienda" style="width: 20%" />
-      <Pv-Column field="image_ingredients_url" header="Ingredientes" style="width: 20%">
-        <template #body="slotProps">
-          <img :src="slotProps.rowData.image_ingredients_url" alt="Imagen de ingredientes" />
-        </template>
-      </Pv-Column>
-      <Pv-Column field="categories_imported" header="Categorías Importadas" style="width: 20%">
-        <template #body="slotProps">
-          <p v-if="slotProps.rowData.categories_imported">{{ slotProps.rowData.categories_imported[0] }}</p>
-        </template>
-      </Pv-Column>
-    </Pv-DataTable>
   </div>
+  
+  
+    <Pv-DataTable :value="data" tableStyle="min-width: 50rem">
+      <Pv-Column field="id" header="Id" sortable style="width: 10%" />
+      <Pv-Column field="name" header="Nombre" style="width: 20%" />
+      <Pv-Column field="status" header="Estado" style="width: 20%" />
+      <Pv-Column field="species" header="Especie" style="width: 20%" />
+      <Pv-Column field="gender" header="Género" style="width: 20%" />
+    </Pv-DataTable>
+
 </template>
 
 <script>
-import { ApiService } from "@/services/api-service";
+import axios from 'axios';
 
 export default {
-  name: "ThePeru",
   data() {
     return {
-      datas: [],
-      dataService: new ApiService(),
+      data: [], // Asegúrate de definir la propiedad 'data' en data
     };
   },
   mounted() {
     this.fetchData();
   },
   methods: {
-    async fetchData() {
-      try {
-        const data = await this.dataService.getAll();
-        this.datas = data;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    },
-    async postData() {
-      try {
-        await this.dataService.postData(this.formData);
-        console.log("Data posted successfully");
-        // Puedes llamar a fetchData() aquí si deseas actualizar la lista después de enviar los datos
-      } catch (error) {
-        console.error("Error posting data:", error);
-      }
-    },
-  },
+    fetchData() {
+      axios.get('https://rickandmortyapi.com/api/character')
+        .then(response => {
+          // Asigna los datos a la propiedad 'data'
+          this.data = response.data.results;
+        })
+        .catch(error => {
+          // Maneja el error aquí
+          console.error('Error al obtener datos de la API', error);
+        });
+    }
+  }
 };
 </script>
